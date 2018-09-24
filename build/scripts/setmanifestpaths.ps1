9390aeea54d880c6b97d5792dd3de3a5f55a7c88
Jonathon Marolf <jmarolf@microsoft.com>
2018-09-24 00:16:37 -0700

    creating bootstrapper

---
+++ build/scripts/setmanifestpaths.ps1
@@ -0,0 +1,9 @@
+# Take care to return nothing if we don't yet have any vsman files
+# as will occur at the start of the build.
+# This will allow us to set it after the build when called again.
+
+$vsmanpath = "Binaries/VSSetup/$(BuildConfiguration)/Insertion"
+if (Test-Path $vsmanpath) {
+    $SetupManifests = [string]::Join(',', (Get-ChildItem "Binaries/VSSetup/$(BuildConfiguration)/Insertion/*.vsman"))
+    Set-Item -Path "env:SetupManifests" -Value $SetupManifests
+}
