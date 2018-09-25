3da8487ab3306da0f3b6c2bbbbfb731c15cbeb09
Jonathon Marolf <jmarolf@microsoft.com>
2018-09-24 17:27:05 -0700

    get build configuration from build environment

--- build/scripts/setmanifestpaths.ps1
+++ build/scripts/setmanifestpaths.ps1
@@ -2,8 +2,8 @@
 # as will occur at the start of the build.
 # This will allow us to set it after the build when called again.
 
-$vsmanpath = "Binaries/VSSetup/$(BuildConfiguration)/Insertion"
+$vsmanpath = "Binaries/VSSetup/$Env:BuildConfiguration/Insertion"
 if (Test-Path $vsmanpath) {
-    $SetupManifests = [string]::Join(',', (Get-ChildItem "Binaries/VSSetup/$(BuildConfiguration)/Insertion/*.vsman"))
+    $SetupManifests = [string]::Join(',', (Get-ChildItem "Binaries/VSSetup/$Env:BuildConfiguration/Insertion/*.vsman"))
     Set-Item -Path "env:SetupManifests" -Value $SetupManifests
 }
